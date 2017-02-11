8b88c8157ab2a8528e6804764d07faf794742be9
alex nichols <alex nichols>
2017-02-10 23:28:03 -0800

    html/javascript update

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+//loads everything below after html is loaded
+$(document).ready(function);
+
+	var triviaGame = {
+
+		correctAnswers: 0,
+		incorrectAnswers: 0,
+		unanswered: 0,
+
+		startGame: function() {
+
+
+		},
+
+		stopGame: function() {
+
+		},
+
+		
+	}
