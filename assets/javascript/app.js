995b8628ee5478a252128e0e344afcf26edc3dfa
alex nichols <alex nichols>
2017-02-14 11:03:32 -0800

    javascript complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	var triviaGame = {
 
 		correctAnswers: 0,
-		incorrectAnswers: 0,
+		wrongAnswers: 0,
 		unanswered: 0,
 		time: 60,
 		setIntervalId: null,
@@ -22,30 +22,98 @@ $(document).ready(function() {
 			$("#timeRemaining").html(triviaGame.time);
 			if (triviaGame.time === 0) {
 				triviaGame.stopGame();
-			}
+			};
 		},
 
 		stopGame: function() {
 
+			if ( $("#correct1").is(":checked")) {
+				this.correctAnswers++;
+				$("#correctAnswers").html(this.correctAnswers);
+			}
+			else if ( $("#wrong1").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong2").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong3").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+
+			if ( $("#correct2").is(":checked")) {
+				this.correctAnswers++;
+				$("#correctAnswers").html(this.correctAnswers);
+			}
+			else if ( $("#wrong4").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong5").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong6").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+
+			if ( $("#correct3").is(":checked")) {
+				this.correctAnswers++;
+				$("#correctAnswers").html(this.correctAnswers);
+			}
+			else if ( $("#wrong7").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong8").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong9").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+
+			if ( $("#correct4").is(":checked")) {
+				this.correctAnswers++;
+				$("#correctAnswers").html(this.correctAnswers);
+			}
+			else if ( $("#wrong10").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong11").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+			else if ( $("#wrong12").is(":checked")){
+				this.wrongAnswers++;
+				$("#wrongAnswers").html(this.wrongAnswers);
+			}
+
 			if( $(".group1").is(":checked") === false) {
 				this.unanswered++;
 				$("#unanswered").html(this.unanswered);
-			}
+			};
 
 			if( $(".group2").is(":checked") === false) {
 				this.unanswered++;
 				$("#unanswered").html(this.unanswered);
-			}
+			};
 
 			if( $(".group3").is(":checked") === false) {
 				this.unanswered++;
 				$("#unanswered").html(this.unanswered);
-			}
+			};
 
 			if( $(".group4").is(":checked") === false) {
 				this.unanswered++;
 				$("#unanswered").html(this.unanswered);
-			}
+			};
 
 			$(".results").show();
 			$(".timer").hide();
